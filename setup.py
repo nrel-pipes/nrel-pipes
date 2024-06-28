276665a90325cfd6ebcb8bf8f054acc9a16b57fd
jeisenma <Jordan.Eisenman@nrel.gov>
2024-06-28 09:17:16 -0600

    commiting setupu.py

--- setup.py
+++ setup.py
@@ -11,16 +11,21 @@ setup(
     description="A Python package for NREL PIPES developers",
     version=__version__,
     author="NREL",
-    author_email="jianli.gu@nrel.gov, jordan.eisenman@nrel.gov",
-    packages=find_namespace_packages(include=["nrel_pipes.*", "pipes_sdk.*", "pipes_cmd.*"]),
-    python_requires=">=3.8",
+    author_email="janisl.gutjrekl.gov, jordan.eisenman@nrel.gov",
+    packages=find_namespace_packages(include=["pipes*", "pipes_sdk*", "pipes_cmd*"]),
+    python_requires=">=3.8.0",
     url="https://github.com/nrel-pipes/nrel-pipes",
     install_requires=requirements,
     include_package_data=True,
-    keywords="nrel,pipes",
+    keywords="nrel pipes",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points={
+        'console_scripts': [
+            'pipes-cli=pipes_cmd.cli:main',
+        ],
+    },
 )
