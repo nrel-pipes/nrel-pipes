dbe892e080a92905ac0ab932007f245fae12f4d8
jeisenma <Jordan.Eisenman@nrel.gov>
2024-08-06 13:03:15 -0400

    installable main pipes_sdk

--- setup.py
+++ setup.py
@@ -6,14 +6,13 @@ def read_requirements():
     with open('requirements.txt') as req_file:
         return req_file.read().splitlines()
 
-
 setup(
     name="nrel-pipes",
     description="A Python package for NREL PIPES developers",
     version=__version__,
     author="NREL",
     author_email="janisl.gu@nrel.gov, jordan.eisenman@nrel.gov",
-    packages=find_namespace_packages(include=["pipes", "pipes.*", "wrapper", "wrapper.*"]),
+    packages=find_namespace_packages(include=["pipes_sdk", "pipes_sdk.*"]),
     python_requires=">=3.8.0",
     url="https://github.com/nrel-pipes/nrel-pipes",
     install_requires=read_requirements(),
@@ -27,7 +26,7 @@ setup(
     entry_points={
         'console_scripts': [
             'pipes=wrapper:main',
-            'pipes-sdk=pipes_sdk:main',
+            'pipes-sdk=pipes_sdk:main',  # New entry point for the SDK
         ],
     },
 )
