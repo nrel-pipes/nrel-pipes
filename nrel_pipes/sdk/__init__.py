98140a0e6716d12bc942ed5f5764d5993d62837f
jeisenma <Jordan.Eisenman@nrel.gov>
2024-08-07 12:49:31 -0400

    HTE

--- nrel_pipes/sdk/__init__.py
+++ nrel_pipes/sdk/__init__.py
@@ -1,5 +1,5 @@
-from .task_engine import *
+from .task_engine import HeroTaskEngine
 from .auth import get_pipes_token, valid_cognito_token, get_hero_token
 from .core import PIPES
 
-__all__ = ["PIPES"]
+__all__ = ["PIPES",  "HeroTaskEngine"]
