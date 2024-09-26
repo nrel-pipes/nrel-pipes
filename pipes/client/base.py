505a717587e0bb8d57fbd353157c7fe1466235a1
jeisenma <Jordan.Eisenman@nrel.gov>
2024-09-26 15:52:17 -0600

    debug

--- pipes/client/base.py
+++ pipes/client/base.py
@@ -52,6 +52,7 @@ class PipesClientBase:
 
     def get(self, url, params=None):
         url = self.host + url
+        print(f"URL: {url}")
         if params:
             try:
                 return requests.get(url, params=params, headers=self.headers)
