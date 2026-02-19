faa8f58d71ea371a15c5590fef10c4012a33b0c5
Morris, James <James.Morris@nrel.gov>
2026-02-19 13:04:16 -0700

    Adding support for YAML/JSON config files
    Adding support to the cli to accept YAML or JSON config files alongside TOML for config files. Maps to one of the three formats based on file name.
    
    Also added support to dump templates in any of the formats.
    
    Adding pyyaml to requirements to support YAML.

--- pipes/template/utils.py
+++ pipes/template/utils.py
@@ -4,6 +4,8 @@ import shutil
 import sys
 
 import toml
+import yaml
+import json
 
 from pipes.utils import print_response
 from pipes.template import TEMPLATE_FILES
@@ -21,26 +23,58 @@ def copy_template(typename, filename, subtype=None):
 
 
 def load_template(filename):
-    """Load data from .toml file"""
+    """Load data from supported file types (TOML/YAML/JSON)"""
     with open(filename) as f:
-        try:
-            data = toml.load(f)
-        except Exception as error:
-            response = {
+        if filename.endswith(".toml"):
+            try:
+                data = toml.load(f)
+            except Exception as error:
+                response = {
+                    "code": "N/A",
+                    "details": f"Failed to load TOML template '{filename}'. {type(error).__name__}: {error}."
+                }
+                print_response(response)
+                sys.exit(1)
+        elif filename.endswith(".yaml") or filename.endswith(".yml"):
+            try:
+                data = yaml.safe_load(f)
+            except yaml.YAMLError as error:
+                response = {
+                    "code": "N/A",
+                    "details": f"Failed to load YAML template '{filename}'. {type(error).__name__}: {error}."
+                }
+                print_response(response)
+                sys.exit(1)
+        elif filename.endswith(".json"):
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError as error:
+                response = {
+                    "code": "N/A",
+                    "details": f"Failed to load JSON template '{filename}'. {type(error).__name__}: {error}."
+                }
+                print_response(response)
+                sys.exit(1)
+        else:
+            print_response({
                 "code": "N/A",
-                "details": f"Failed to load template '{filename}'. {type(error).__name__}: {error}."
-            }
-            print_response(response)
+                "details": f"Unsupported template file type for '{filename}'. Supported types are: .toml, .yaml/.yml, .json."
+            })
             sys.exit(1)
 
     return data
 
 
 def dump_template(data, filename):
-    """Dump data to .toml file"""
+    """Dump data to .toml/.yaml/.json file"""
     with open(filename, "w") as f:
         try:
-            toml.dump(data, f)
+            if filename.endswith(".toml"):
+                toml.dump(data, f)
+            elif filename.endswith(".yaml") or filename.endswith(".yml"):
+                yaml.safe_dump(data, f)
+            elif filename.endswith(".json"):
+                json.dump(data, f)
         except Exception as error:
             response = {
                 "code": "N/A",
